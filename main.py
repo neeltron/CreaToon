cbef5ed86c9d915d5add6403f4f6423961790b19
Neel Adwani <nprosofficial@gmail.com>
2021-07-10 03:09:06 +0000

    Added Adaptive Threshold

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ cv2.imwrite('input.jpg', img)
 
 edges = cv2.Canny(img, 100, 200)
 cv2.imwrite('canny.jpg', edges)
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 15)
+
+cv2.imwrite('adthresh.jpg', edges)
