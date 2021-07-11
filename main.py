bc907c672c0b47d40914153d49f599cb4b5a5f8d
Neel Adwani <nprosofficial@gmail.com>
2021-07-11 01:38:07 +0000

    Omitted

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, request, make_response, redirect, url_for
+from flask import Flask, request
 import cv2
-import numpy as np
-import urllib.request
 from PIL import Image
 import requests
 from io import BytesIO
